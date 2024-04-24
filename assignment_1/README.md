ab3c90d3296571c475e908f3fc9601f0e27a0e3b
Rasheed Ellis <rasheed.ellis@incentro.com>
2024-04-24 18:01:22 +0200

    Update README and added the complete assignment-1

---
+++ assignment_1/README.md
@@ -0,0 +1,6 @@
+# Assignment 1
+
+For this assignment, modify the application to display any word of your choice on the webpage. Once you have modified the application code, deploy the changes by running the pipline.    
+
+> [!TIP]
+> Take a look at `app.py` 
---
+++ assignment_1/a1-pipeline.py
@@ -0,0 +1,34 @@
+import subprocess
+
+container_name = "assignment-1"
+
+
+def docker_build():
+    try:
+        # Run 'docker build' command to build the Docker image from the current directory
+        subprocess.run(["docker", "build", "-t", container_name, "."], check=True)
+        print("Docker build successful.")
+    except subprocess.CalledProcessError as e:
+        print("Error: Docker build failed.")
+        print(e)
+
+
+def docker_run():
+    try:
+        # Run 'docker run' command to run the built Docker image
+        subprocess.run(
+            ["docker", "run", "-p", "5000:5000", "-d", container_name], check=True
+        )
+    except subprocess.CalledProcessError as e:
+        print("Error: Docker run failed.")
+        print(e)
+
+
+if __name__ == "__main__":
+
+    print("Triggering deployment of application! :^) ")
+    # Build the Docker image
+    docker_build()
+
+    # Run the Docker container
+    docker_run()
