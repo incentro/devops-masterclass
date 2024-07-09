6224b5f40e914aec8737129d03c2b879a407c600
Tim Bosua <tim.bosua@incentro.com>
2024-07-09 10:04:25 +0200

    Add assignment 5

---
+++ assignment_5/a5-pipeline.py
@@ -0,0 +1,19 @@
+import subprocess
+
+
+def docker_compose():
+    try:
+        print("Performing docker compose deployments")
+
+        # Run 'docker compose' command to build the Docker image from the current directory
+        subprocess.run(["docker-compose", "up", "--build"], check=True)
+    except subprocess.CalledProcessError as e:
+        print("Error: Docker compose failed.")
+        print(e)
+
+
+if __name__ == "__main__":
+
+    print("Triggering deployment of applications! :^) ")
+    # Build the Docker image
+    docker_compose()
